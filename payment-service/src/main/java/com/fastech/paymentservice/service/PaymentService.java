8799c94d3815ceb225245e125e732b17eeb7df5a
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-30 15:26:59 +0500

    random id will be auto generated

--- payment-service/src/main/java/com/fastech/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/fastech/paymentservice/service/PaymentService.java
@@ -5,12 +5,15 @@ import com.fastech.paymentservice.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
     public Payment doPayment(Payment payment) {
+        payment.setTransactionId(UUID.randomUUID().toString());
         return paymentRepository.save(payment);
     }
 
