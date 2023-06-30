874f8cf2ca65177c97f1d637987b853b8c55f6e4
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-30 21:49:37 +0500

    Error resolved!

--- order-service/src/main/java/com/fastech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/fastech/orderservice/service/OrderService.java
@@ -17,23 +17,92 @@ public class OrderService {
     private OrderRepository orderRepository;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private RestTemplate template;
+
+
 
     public TransactionResponse saveOrder(TransactionRequest request){
 
-        String response="";
+        String response = "";
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
-        //rest call
-        Payment paymentResponse = restTemplate.postForObject("http://localhost:9091/payment/doPayment",payment,Payment.class);
 
-        response =paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"There is a failure in payment api";
+        Payment paymentResponse = template.postForObject("http://localhost:9091/payment/doPayment",payment,Payment.class);
+
+        response = paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"there is a failure in payment api";
 
         orderRepository.save(order);
-        return new TransactionResponse(order,paymentResponse.getAmount(),paymentResponse.getTransactionId(),response);
+        return new TransactionResponse(order,paymentResponse.getAmount(),paymentResponse.getTransactionId(),response) ;
     }
 
+
+
+
+
+
+
+//
+//
+//    public TransactionResponse saveOrder(TransactionRequest request){
+//
+//        String response = "";
+//        Order order = request.getOrder();
+//        Payment payment = request.getPayment();
+////
+////        if (order != null) {
+////            payment.setOrderId(order.getId());
+////            payment.setAmount(order.getPrice());
+////        } else {
+////            // Handle the case when order is null
+////            // For example, you could throw an exception or return an error response
+////            // depending on your application's requirements.
+////            // ...
+////        }
+////
+//////        Payment paymentResponse = template.postForObject("http://localhost:9091/payment/doPayment",payment,Payment.class);
+////
+////
+////
+//////        orderRepository.save(order);
+//////        return new TransactionResponse(order,paymentResponse.getAmount(),paymentResponse.getTransactionId(),response);
+////
+////
+//        TransactionResponse obj = null;
+//        try {
+//
+//            Payment paymentResponse = template.postForObject("http://localhost:9091/payment/doPayment", payment, Payment.class);
+//            System.out.println("Hello2");
+//            response =paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"There is a failure in payment api";
+//
+//
+//            if (paymentResponse != null) {
+//                System.out.println("Hello");
+//                response = "Payment processing successful and order placed";
+//            } else {
+//                response = "There is a failure in the payment API";
+//            }
+//
+//            orderRepository.save(order);
+//            System.out.println("Hello");
+//            obj = new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
+//            return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
+//        } catch (Exception e) {
+//            // Handle exceptions that may occur during the REST call
+//            // For example, you could log the error or return an appropriate error response
+//            // depending on your application's requirements.
+//            // ...
+//
+//        }
+//
+//         // Return an appropriate response or throw an exception if necessary
+//        return obj;
+//    }
+//
+//
+//
+//
+//
 
 }
