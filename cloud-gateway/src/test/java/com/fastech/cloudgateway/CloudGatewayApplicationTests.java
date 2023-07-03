e76130de442d3f7ed08c8b950db0eefeca8821f3
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-07-03 11:12:06 +0500

    Added Gateway file

---
+++ cloud-gateway/src/test/java/com/fastech/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastech.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
