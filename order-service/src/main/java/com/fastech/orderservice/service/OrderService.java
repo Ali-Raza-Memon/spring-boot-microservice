33e105135aadb4d3a7bd0c9af56fa16ece63b927
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-07-03 10:29:42 +0500

    Throw an unknownHostException

--- order-service/src/main/java/com/fastech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/fastech/orderservice/service/OrderService.java
@@ -29,7 +29,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
 
-        Payment paymentResponse = template.postForObject("http://localhost:9091/payment/doPayment",payment,Payment.class);
+        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment",payment,Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success")?"payment processing successful and order placed":"there is a failure in payment api";
 
